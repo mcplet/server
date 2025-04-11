a7811a886fbd4e23cce7b0e0fd27750779b48c80
Mcplet DEV <info@mcplet.com>
2025-04-11 22:22:23 +0300

    version 1.1.0

---
+++ dist/index.js
@@ -0,0 +1,73 @@
+import { Server } from "@modelcontextprotocol/sdk/server/index.js"; 
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {
+  ListPromptsRequestSchema,
+  GetPromptRequestSchema,
+  ListToolsRequestSchema,
+  CallToolRequestSchema,
+  ListResourcesRequestSchema
+} from "@modelcontextprotocol/sdk/types.js";
+import fetch from 'node-fetch';
+
+
+// Parse command line arguments
+const args = process.argv.slice(2);
+const uid = args[0];
+
+if (!uid) {
+  process.exit(1);
+}
+
+const REMOTE_BASE_URL = 'https://www.mcplet.com/api/protocol';
+
+async function fetchRemoteData(endpoint) {
+  try {
+    const fetchurl = `${REMOTE_BASE_URL}/${uid}/${endpoint}`;
+    const response = await fetch(fetchurl);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    const json  = await response.json();
+    return json;
+  } catch (error) {
+    return [];
+  }
+}
+
+const server = new Server(
+  {
+    name: "mcplet-mcp-server",
+    version: "1.0.0"
+  },
+  {
+    capabilities: {
+      tools: {}
+    }
+  }
+);
+
+// Define available tools
+server.setRequestHandler(ListToolsRequestSchema, async () => {
+  const tools = await fetchRemoteData('');
+  return (tools);
+});
+
+// Handle tool execution
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+  const toolname = request.params.name;
+  const invokeRes = await fetchRemoteData(`invoke?method=${toolname}`);
+  return invokeRes;
+});
+
+async function main() {
+  try {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+  } catch (error) {
+
+  }
+}
+
+main();
